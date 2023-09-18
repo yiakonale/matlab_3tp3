7d75ec5696bab8ae9742285ebfea3828f04c47b2
yiakonale <yiakonale@hotmail.com>
2023-09-18 10:47:12 -0400

    assignment 1

--- assignment1.m
+++ assignment1.m
@@ -11,4 +11,11 @@ n = -10:10;
 %c. x[n] = δ[n + 1] − δ[n] + u[n + 1] − u[n − 2]
 %ex = f.delta(n + 1) - f.delta(n) + f.unitstep(n+1) - f.unitstep(n - 2);
 
+%d. x[n] = e^(0.8n)u[n + 1] + u[n]
+%ex = exp(0.8*n).*f.unitstep(n+1) + f.unitstep(n);
+
 stem(n, ex, "LineWidth", 3);
+%xlim([0 10]);
+%ylim([-0.4 0.8]);
+
+%exportgraphics(gcf, %filename_you_want.jpg’);
