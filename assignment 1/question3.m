d90a6e1f0a906eeb197b2723cf7c6203b727cd8c
yiakonale <yiakonale@hotmail.com>
2023-09-20 12:12:03 -0400

    q3

---
+++ assignment 1/question3.m
@@ -0,0 +1,16 @@
+img = imread("ee3tp3picture2023.jpg");
+
+r = img(:,:,1);
+g = img(:,:,2);
+b = img(:,:,3);
+
+r_scale = 4.92;
+g_scale = 4.53;
+
+rec_r = r*r_scale;
+rec_g = g*g_scale;
+
+reconstructed_image = cat(3, rec_r, rec_g, b);
+
+imshow(reconstructed_image);
+imwrite(reconstructed_image, "my_fixed_image.jpg");
